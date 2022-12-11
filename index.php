04712e6a0f3a828a16178793f00a6a52e9cb58a9
skyburg <gli466731@gmail.com>
2022-12-11 21:11:55 +0800

    上传

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>登录页面</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./css/index.css">
+	<style type="text/css">
+
+    </style>
+    <script >
+        function forget(){
+            alert("忘了就忘了吧，人生总有遗憾")
+            return false;
+        };
+    </script>
+</head>
+<body>
+<div class="main">
+        <div class="login-form">
+            <h1>留言板登录</h1>
+            <div class="head">
+                <img src="images/user.jpg" alt="" />
+            </div>
+            <form method="post" action="loaddemo.php" name="frmlogin"> 
+                <input placeholder="请输入用户名" type="text" class="text" value="用户名" name="loginname" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '请输入用户名';}">
+                <input placeholder="请输入密码" type="password" value="密码" name="password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}"><hr>
+                <div class="submit">
+                    <input type="submit" name="denglu" onclick="myFunction()" value="登录">
+					<input type="button" name="zhuce" value="注册" onclick="window.location.href='register.php'">
+                </div>
+                <p><a href="#" onclick="forget();"> 忘记密码 ?</a></p>
+            </form>
+        </div>
+
+        <div class="copy-right">
+            <p>Be made by<a href="http://www.skyburg.top"> 空城</a></p>
+        </div>
+
+    </div>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>注册帐号</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./css/index.css">
+	<style type="text/css">
+		body:{height: 100%;}
+	</style>
+</head>
+<body>
+
+	<div class="main">
+        <div class="login-form">
+            <h1>注册界面</h1>
+            <div class="head">
+                <img src="images/user.jpg" alt="" />
+            </div>
+            <form action="registerdemo.php " name="dl" method="post"> 
+                <input type="name" name="id" placeholder="手机号/邮箱" style="width:180px;font-size:20px;border-radius: 8px; width: 93%;">
+
+                <input name="password" type="password"  placeholder="请输入密码" style="width:180px;font-size:20px;border-radius: 8px; width: 93%;"">
+
+				<input name="secondpassword" type="password"  placeholder="请再次输入密码" style="width:180px;font-size:20px;border-radius: 8px;width: 93%;" ">
+
+                <div class="submit">
+                    <input type="button" name='zu' value='登陆' onclick="window.location.href='index.php'" >
+	        <input type="submit" name="zu" value="注册" >
+                </div>
+                
+            </form>
+        </div>
+
+        <div class="copy-right">
+            <p>Be made by<a href="http://www.skyburg.top"> 空城</a></p>
+        </div>
+
+    </div>
+</body>
+</html>
